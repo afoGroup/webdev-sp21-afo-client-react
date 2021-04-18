5b089e904b2c418559906c1ca5f266476c2a4df8
thomasdiaz1 <tomdiaz@Toms-MacBook-Air.local>
2021-04-17 23:46:21 -0500

    additional methods added to user service per changes in user controller on server side

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -4,25 +4,48 @@ export const registerUser = (user) =>
     fetch(`${USER_URL}/api/register`, {
         method: "POST",
         body: JSON.stringify(user),
+        credentials: "include",
         headers: {
             'content-type' : 'application/json'
         }
     })
         .then(response => response.json())
 
+export const profile = () =>
+    fetch(`${USER_URL}/api/profile`, {
+        method: "POST",
+        credentials: "include"
+    })
+        .then(response => response.json())
+
+export const login = (user) =>
+    fetch(`${USER_URL}/api/login`, {
+        method: "POST",
+        body: JSON.stringify(user),
+        credentials: "include",
+        headers: {
+            'content-type' : 'application/json'
+        }
+    })
+        .then(response => response.json())
+
+export const logout = () =>
+    fetch(`${USER_URL}/api/logout`, {
+        method: "POST",
+        credentials: "include"
+    })
+        .then(response => response.json())
 
 export const findAllUsers = () =>
     fetch(`${USER_URL}/api/users`).then(response => response.json())
-
 
 
 export const findUserById = (uid) =>
     fetch(`${USER_URL}/api/user/${uid}`).then(response => response.json())
 
 
-
 const api = {
-    registerUser, findAllUsers, findUserById
+    registerUser, findAllUsers, findUserById, profile, logout, login
 }
 
 export default api;
