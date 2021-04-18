815d37a809ac480fe41c0a454a2751467bb5c0a8
thomasdiaz1 <tomdiaz@Toms-MacBook-Air.local>
2021-04-18 11:57:40 -0500

    user login and registration base added. Need to be reformatted to match website design pattern. Registration a bit slow

--- src/components/login.js
+++ src/components/login.js
@@ -1,11 +1,49 @@
-import React from "react";
+import React,{useState} from "react";
+import {Link, useHistory} from 'react-router-dom'
+import {connect} from 'react-redux';
+import userActions from "../actions/user-actions";
 
-const Login = () => {
+const Login = ({loginUser}) => {
+    const [credentials, setCredentials] = useState({username: '', password: ''})
+    const history = useHistory()
+    const login = () => {
+        loginUser(credentials)
+        history.push('/home')
+    }
+
     return(
         <div>
             <h1>Login</h1>
+            <input
+                value={credentials.username}
+                onChange={(e) => {setCredentials({...credentials, username: e.target.value})}}
+                className="form-control"
+                placeholder="username"/>
+            <input
+                value={credentials.password}
+                onChange={(e) => {setCredentials({...credentials, password: e.target.value})}}
+                className="form-control"
+                placeholder="password"/>
+            <button
+                onClick={login}
+                className="btn btn-primary">
+                Login
+            </button>
+            <Link to="/registration">
+                Register
+            </Link>
         </div>
     )
 };
 
-export default Login;
+const stateToPropertiesManager = (state) => ({
+    user: state.userReducer.user
+})
+
+const dispatchToPropertiesManager = (dispatch) => ({
+    loginUser: (user) => userActions.loginUser(dispatch, user)
+})
+
+export default connect(stateToPropertiesManager, dispatchToPropertiesManager)(Login);
+
+
--- src/components/registration.js
+++ src/components/registration.js
@@ -1,11 +1,47 @@
-import React from "react";
+import React,{useState} from "react";
+import {Link, useHistory} from 'react-router-dom';
+import {connect} from 'react-redux';
+import userActions from "../actions/user-actions";
 
-const Registration = () => {
+const Registration = ({registerUser}) => {
+    const [credentials, setCredentials] = useState({username: '', password: ''})
+    const history = useHistory()
+    const register = () => {
+        registerUser(credentials)
+        history.push('/home')
+    }
+
     return(
         <div>
             <h1>Registration</h1>
+            <input
+                value={credentials.username}
+                onChange={(e) => {setCredentials({...credentials, username: e.target.value})}}
+                className="form-control"
+                placeholder="username"/>
+            <input
+                value={credentials.password}
+                onChange={(e) => {setCredentials({...credentials, password: e.target.value})}}
+                className="form-control"
+                placeholder="password"/>
+            <button
+                onClick={register}
+                className="btn btn-primary">
+                Register
+            </button>
+            <Link to="/login">
+                Login
+            </Link>
         </div>
     )
 };
 
-export default Registration;
+const stateToPropertiesManager = (state) => ({
+    user: state.userReducer.user
+})
+
+const dispatchToPropertiesManager = (dispatch) => ({
+    registerUser: (user) => userActions.registerUser(dispatch, user)
+})
+
+export default connect(stateToPropertiesManager, dispatchToPropertiesManager)(Registration);
