2226de8e99e9be528c9aed56e8ceb357f6cad776
Kristin White <kristinwhite103@gmail.com>
2021-04-26 17:02:19 -0400

    uncomment

--- src/constants/api-urls.js
+++ src/constants/api-urls.js
@@ -1,5 +1,3 @@
 export const ANIME_URL = "https://api.jikan.moe/v3";
 export const WHAT_ANIME_URL = "https://trace.moe/api/search";
-
-//export const ANIFANSONLY_URL = "https://anifansonly-server-node.herokuapp.com";
-export const ANIFANSONLY_URL = "";
+export const ANIFANSONLY_URL = "https://anifansonly-server-node.herokuapp.com";
