f6fc90fdff19a3c23b4cb195fafba5bce82e660d
tchen007 <46579382+tchen007@users.noreply.github.com>
2021-04-26 03:36:39 -0400

    also needs to handle undefined

--- src/components/navbar/afo-navbar.js
+++ src/components/navbar/afo-navbar.js
@@ -31,7 +31,7 @@ const AfoNavbar = () => {
         userService.getCurrentUser()
             .then((actualUser) => {
                 console.log("(navabr): " + actualUser);
-                if(actualUser.username === "wbdv-afo-logged-out"){
+                if(actualUser === undefined || actualUser.username === "wbdv-afo-logged-out"){
                     setLoginState(LOGIN_STATE.LOGGED_OUT)
                 } else {
                     console.log("(navbar) user: " + actualUser.username + " & " + actualUser._id);
