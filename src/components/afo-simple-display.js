011cab6cd29302f1c5fbb58043f7eb40d859098e
Kristin White <kristinwhite103@gmail.com>
2021-04-27 21:25:12 -0400

    updating

--- src/components/afo-simple-display.js
+++ src/components/afo-simple-display.js
@@ -7,27 +7,34 @@ const SimpleDisplay = ({imageURL, header, text, linkId, type}) => {
     return(
         <div className="row afo-simple-container">
             <div className="col-5">
-                <img
-                    src={imageURL}
-                    className="afo-simple-img"
-                    alt={`${header}`}/>
+                <div className="row">
+                    <div className="col-12">
+                        <img
+                            src={imageURL}
+                            className="afo-simple-img"
+                            alt={`${header}`}/>
+                    </div>
+                </div>
             </div>
             <div className="col-7 afo-simple-info">
-                <p className="afo-purple">{header}</p>
-                <p className="afo-simple-text">{text}</p>
-                {
-                    type === 'group' &&
-                    <Link className="btn btn-secondary" to={`/details/group/${linkId}`}>
-                        View Group
-                    </Link>
-                }
-                {
-                    type === 'user' &&
-                    <Link className="btn btn-secondary" to={`/details/anime/${linkId}`}>
-                        View Group
-                    </Link>
-                }
-
+                <div className="row">
+                    <div className="col-12">
+                        <p className="afo-purple">{header}</p>
+                        <p className="afo-simple-text">{text}</p>
+                        {
+                            type === 'group' &&
+                            <Link className="btn btn-secondary" to={`/details/group/${linkId}`}>
+                                View Group
+                            </Link>
+                        }
+                        {
+                            type === 'user' &&
+                            <Link className="btn btn-secondary" to={`/details/anime/${linkId}`}>
+                                View Group
+                            </Link>
+                        }
+                    </div>
+                </div>
             </div>
         </div>
     )
